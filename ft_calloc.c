ea06c65cf395a673d8795095e470bc448b181996
Lautaro Javier Fernandez Pricco <lauferna@student.42.fr>
2025-09-25 23:49:12 +0200

    Update ft_calloc. to account for a stupid rule.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,22 +6,24 @@
 /*   By: lauferna <ljfp@ljfp.xyz>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 20:56:11 by lauferna          #+#    #+#             */
-/*   Updated: 2025/09/25 23:11:57 by lauferna         ###   ########.fr       */
+/*   Updated: 2025/09/25 23:48:28 by lauferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// if t == 0 we have to still create a pointer because of a
+// stupid rule in the subject.
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	t;
 	void	*p;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
 	if (size != 0 && nmemb > SIZE_MAX / size)
 		return (NULL);
 	t = nmemb * size;
+	if (t == 0)
+		t = 1;
 	p = malloc(t);
 	if (!p)
 		return (NULL);
